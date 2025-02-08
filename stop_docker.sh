d5d3b4d95d48a9ba3a67841bce65f64f11459784
naren200 <narendhiran2000@gmail.com>
2025-02-07 20:52:12 -0700

    Make the docker 60% SLIM

--- stop_docker.sh
+++ stop_docker.sh
@@ -3,6 +3,7 @@
 # this has to be non-empty for docker-compose to run, even for down
 # these don't have to be set to anything correct
 export LOCAL_WS="/"
+export CAPTURE_DEVICE="/"
 
 # Stop and remove the Docker Compose services
 docker compose down --remove-orphans
